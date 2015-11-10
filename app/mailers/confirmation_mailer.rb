28221ef060b7a9ab66c19bd6d356d7aaab2cb4a4
Ahsan Malik <sann.lix@gmail.com>
2015-11-10 10:08:17 +0700

    Task 5 v4

--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -1,3 +1,18 @@
+require "action_mailer"
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 465,
+  :domain               => '@gmail.com',
+  :user_name            => 'devror4',
+  :password             => 'persibmaungbandung1933',
+  :authentication       => "login",
+  :ssl                  => true,
+  :tls                  => true,
+  :enable_starttls_auto => true
+}
+    
 class ConfirmationMailer < ActionMailer::Base
   def confirm_email(target_email, activation_token)
     @activation_token = activation_token
